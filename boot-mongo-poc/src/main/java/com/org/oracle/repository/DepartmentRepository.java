c5ed39d56977afd24a0dba59acea3b5a9a13180d
subrata10 <subratasikdar10@gmail.com>
2019-06-24 23:21:42 +0530

    small modification

--- boot-mongo-poc/src/main/java/com/org/oracle/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.org.oracle.repository;
-
-import com.org.oracle.domain.Department;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DepartmentRepository extends MongoRepository<Department, String> {
-}
--- boot-mongo-poc/src/main/java/com/org/oracle/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.org.oracle.repository;
-
-import com.org.oracle.domain.Employee;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EmployeeRepository extends MongoRepository<Employee, String> {
-}
