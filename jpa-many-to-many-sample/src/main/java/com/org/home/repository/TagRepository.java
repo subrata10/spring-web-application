2ff8473246fb05b7e63e3b7c3f61dce560f5fbea
subrata10 <subratasikdar10@gmail.com>
2019-06-24 23:08:35 +0530

    small changes

---
+++ jpa-many-to-many-sample/src/main/java/com/org/home/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package com.org.oracle.repository;
+
+import com.org.oracle.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
